8830567301ab6f98ab861c8770270f7177c05908
aimForStars <96152325+aimForStars@users.noreply.github.com>
2021-12-14 23:47:43 -0500

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("../Design.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("Design.fxml"));
 		Scene scene = new Scene(root, 300, 275);
 	    
         stage.setTitle("FXML Welcome");
