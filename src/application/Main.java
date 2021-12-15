801916584f3c15ece3113c35a86f6a52ce12aece
aimForStars <96152325+aimForStars@users.noreply.github.com>
2021-12-14 23:47:19 -0500

    start

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,30 @@
 package application;
-	
+
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.*;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("../Design.fxml"));
+		Scene scene = new Scene(root, 300, 275);
+	    
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();		//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
+
+	}
+
 }
